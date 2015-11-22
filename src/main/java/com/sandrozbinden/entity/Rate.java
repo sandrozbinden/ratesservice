2e85f0a2faa4c7ee2ad266064d8f47d7c276db7a
Sandro Zbinden <sandrozbinden@gmail.com>
2015-11-22 18:17:54 +0100

    Overwrite equals and hashcode for Rate

--- src/main/java/com/sandrozbinden/entity/Rate.java
+++ src/main/java/com/sandrozbinden/entity/Rate.java
@@ -1,12 +1,16 @@
 package com.sandrozbinden.entity;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.joda.time.LocalDate;
+
 public class Rate {
 
-	private String date;
+	private LocalDate date;
 	private String currency;
 	private double rate;
 
-	public Rate(String currency, double rate, String date) {
+	public Rate(String currency, double rate, LocalDate date) {
 		this.currency = currency;
 		this.rate = rate;
 		this.date = date;
@@ -20,8 +24,18 @@ public class Rate {
 		return rate;
 	}
 
-	public String getDate() {
+	public LocalDate getDate() {
 		return date;
+	}
+
+	@Override
+	public boolean equals(Object rhs) {
+		return EqualsBuilder.reflectionEquals(this, rhs);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 }
