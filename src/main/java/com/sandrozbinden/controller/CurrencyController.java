4f58e0cbd5a3c042297bb7f80d015897a50c76b1
Sandro Zbinden <sandrozbinden@gmail.com>
2015-11-22 19:03:37 +0100

    Add possiblity to get rates from the past through the history resource
    Example /api/rates/history/eur/2015-11-20

--- src/main/java/com/sandrozbinden/controller/CurrencyController.java
+++ src/main/java/com/sandrozbinden/controller/CurrencyController.java
@@ -1,7 +1,9 @@
 package com.sandrozbinden.controller;
 
 import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +21,8 @@ public class CurrencyController {
 		return euroCurrencyService.getRates(LocalDate.now());
 	}
 
+	@RequestMapping("/api/rates/history/eur/{date}")
+	public Currency getCurrency(@PathVariable String date) {
+		return euroCurrencyService.getRates(DateTimeFormat.forPattern("yyyy-MM-dd").parseLocalDate(date));
+	}
 }
