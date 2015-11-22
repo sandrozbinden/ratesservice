2e356116776f1b2714f2afe02b348b77ac2b3174
Sandro Zbinden <sandrozbinden@gmail.com>
2015-11-22 15:51:26 +0100

    Create project skeleton

---
+++ src/main/java/com/sandrozbinden/RatesserviceApplication.java
@@ -0,0 +1,12 @@
+package com.sandrozbinden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatesserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatesserviceApplication.class, args);
+    }
+}
